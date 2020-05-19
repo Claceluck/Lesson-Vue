95d7547d90199ebc93e5016e789922e99e7a41cf
Philip <heroesfill@gmail.com>
2020-05-19 23:51:39 +0300

    ++

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
+<v-app >
+    <v-row class="fill-height">
         <!--неизменная часть-->
-        <div>
+        <!-- <div>
             <p>Всего задач: {{tasks.length}}</p>
             <nav>
                 <router-link to="/add/task">
@@ -11,27 +12,94 @@
                     Просмотр задач
                 </router-link>
             </nav>
-        </div>
+        </div> -->
+        <v-navigation-drawer
+                    v-resize="onResize"
+                    v-model="drawer"
+                    :mini-variant.sync="mini"
+                    permanent>
 
-        <!--подгружаемый контент-->
-        <div>
-            <router-view></router-view>
-        </div>
-    </div>
+                <v-list-item>
+                    <v-list-item-avatar>
+                        <v-img :src="src"></v-img>
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                        <v-list-item-title>LOGIN</v-list-item-title>
+                        <v-list-item-subtitle>Всего задач: {{tasks.length}}</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-divider></v-divider>
+
+                <v-list>
+                    <v-list-item>
+                        <v-list-item-action>
+                            <v-icon>mdi-movie</v-icon>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                <router-link class="text-uppercase text--secondary" to="/add/task">
+                                    Создать задачу
+                                </router-link>
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+
+                    <v-list-item>
+                        <v-list-item-action>
+                            <v-icon>mdi-movie</v-icon>
+                        </v-list-item-action>
+
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                <router-link class="text-uppercase text--secondary" to="/show/tasks">
+                                    Просмотр задач
+                                </router-link>
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+
+
+                </v-list>
+
+            </v-navigation-drawer>
+
+       <!--подгружаемый контент-->
+            <v-content>
+                <v-container class="fill-height">
+                    <router-view class="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8"></router-view>
+                </v-container>
+            </v-content>
+        </v-row>
+    </v-app>
 </template>
 
 <script>
     import {allTasks} from "./tmp/taskArr";
     export default {
-        name: "App",
+        name: "App", //имя файла без расширения
         data: function () {
             return {
-                tasks: allTasks
+                tasks: allTasks,
+                drawer: null,
+                mini: false,
+                // src: "https://picsum.photos/id/237/300"
+                src: "./static/img/cat-pic.png"
+            }
+        },
+        mounted(){
+            this.onResize()
+        },
+        methods: {
+            onResize: function () {
+                console.log(window.innerWidth)
+                this.mini = window.innerWidth < 850;
             }
         }
     }
 </script>
 
 <style lang="scss">
-
 </style>
--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -3,10 +3,32 @@
        <p>{{outside}}</p>
 
        <form @submit.prevent="addTask">
-           <input type="text" name="title" v-model.trim="title">
+           <!-- <input type="text" name="title" v-model.trim="title"> -->
+           <v-text-field
+            single-line
+            label="Название статьи"
+            name="title"
+            required
+            v-model.trim="title"
+            :rules="titleRules"
+            hide-details="auto"
+            ></v-text-field>
            <input type="datetime-local" name="date" v-model="date">
-           <textarea name="description" v-model.trim="description"></textarea>
-           <input type="submit" value="Добавить">
+           <!-- <textarea name="description" v-model.trim="description"></textarea> -->
+           <v-textarea
+           outlined
+           label="Описание задачи"
+           name="description"
+           required
+           v-model.trim="description"
+           :rules="descriptionRules"
+           hide-details="auto"></v-textarea>
+           <!-- <input type="submit" value="Добавить"> -->
+           <v-btn
+           color="primary"
+           class="mt-5"
+           type="sumbit"
+           >Добавить</v-btn>
        </form>
 
    </div>
@@ -24,7 +46,15 @@
                 title: "",
                 date: "",
                 description: "",
-                tasks: allTasks
+                tasks: allTasks,
+                titleRules: [
+                    v => v.length >= 2 || 'От 2 символов',
+                    v => v.length <= 40 || 'До 40 символов'
+                ],
+                descriptionRules: [
+                    v =>  v.length >= 20 || 'От 20 символов',
+
+                ]
             }
         },
         methods: {
